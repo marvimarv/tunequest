d826562cc83a63ec9d9df6580a43b5a8bf0da942
Marvin Hackfort <marvin.hackfort@gmail.com>
2025-04-19 12:02:40 +0200

    changed url

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,7 +28,7 @@ export default function Home() {
                                 Spiel starten
                             </Link>
                             <Link
-                                href="/REPLACEME_CREATE_URL"
+                                href="/tunequest-card-generator/"
                                 target="_blank"
                                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                             >
